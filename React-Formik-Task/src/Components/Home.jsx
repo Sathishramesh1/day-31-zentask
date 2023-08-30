313b834c321cb7441b8af286d7fc90cf1d0691ba
Sathishramesh1 <sathishrameshkec@gmail.com>
2023-08-30 23:18:29 +0530

    commit-basic-layout

---
+++ React-Formik-Task/src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='home'>
+        <h3>Welcome to Library</h3>
+        <h5>
+            To Perform CRUD Operation in Book Go to Book page in sidebar
+        </h5>
+        <h5>
+        To Perform CRUD Operation in Author Go to Author page in sidebar
+        </h5>
+        
+
+
+    </div>
+  )
+}
+
+export default Home
